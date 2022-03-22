45ed7af5934a2b436210e6557c4815ff4725167d
ShanchaiZ <shanchai.zahid@gmail.com>
2022-03-21 20:15:22 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+//Required libraries
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/SocialMediaApi', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//This code below is used tolog mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`App is connected on localhost:${PORT}`));
